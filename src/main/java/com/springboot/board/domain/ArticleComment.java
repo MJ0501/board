0011e7bab276526b79793cc6b43e10ef1a08b99a
MJ0501 <mjay.do@gmail.com>
2024-11-27 14:29:20 +0900

    Auto stash before merge of "main" and "origin/main"

--- src/main/java/com/springboot/board/domain/ArticleComment.java
+++ src/main/java/com/springboot/board/domain/ArticleComment.java
@@ -1,11 +1,32 @@
 package com.springboot.board.domain;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+import java.time.LocalDateTime;
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+})
+@Entity
 public class ArticleComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Article article;    // 게시글ID
+
+    @Setter
+    @Column(nullable = false)
+    private Article article;// 게시글ID
+
+    @Setter
+    @Column(nullable = false)
     private String content;
+
     private LocalDateTime createdAt;
     private String createdBy;
     private LocalDateTime modifiedAt;
