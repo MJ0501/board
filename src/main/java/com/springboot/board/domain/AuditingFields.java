3b738a2f9fd4035ee533baf4aa2b946fcf8cd1d0
MJ0501 <mjay.do@gmail.com>
2024-11-27 18:22:12 +0900

    domain 에서 AuditingFields 추출
    createdAt, createdBy, modifiedAt, modifiedBy
    + DataTimeFormat 사용

---
+++ src/main/java/com/springboot/board/domain/AuditingFields.java
@@ -0,0 +1,39 @@
+package com.springboot.board.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+@ToString
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class AuditingFields {
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false, updatable = false, length = 100)
+    @CreatedBy
+    private String createdBy;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    @Column(nullable = false, length = 100)
+    @LastModifiedBy
+    private String modifiedBy;
+}
