caaab7195ab82e7757108649b95bf71105470fb2
MJ0501 <mjay.do@gmail.com>
2024-12-11 19:31:47 +0900

    TODO: 구현해야할 로직과 view 설정
    service, controller

--- src/main/java/com/springboot/board/controller/ArticleController.java
+++ src/main/java/com/springboot/board/controller/ArticleController.java
@@ -1,9 +1,14 @@
 package com.springboot.board.controller;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -14,5 +19,18 @@ public class ArticleController {
     public String articles(ModelMap map){
         map.addAttribute("articles", List.of());
         return "articles/index";
+    }
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map) {
+        map.addAttribute("article", "article"); // TODO: 구현할 때 여기에 실제 데이터를 넣어줘야 한다
+        map.addAttribute("articleComments", List.of());
+        return "articles/detail";
+    }
+    @GetMapping("/search-hashtag")
+    public String searchHashtag(@RequestParam(required = false) String searchValue,
+                                @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+                                ModelMap map) {
+        //TODO: search-hashtag view controller
+        return "articles/search-hashtag";
     }
 }
---
+++ src/main/java/com/springboot/board/service/ArticleService.java
@@ -0,0 +1,49 @@
+package com.springboot.board.service;
+
+import com.springboot.board.domain.constant.SearchType;
+import com.springboot.board.dto.ArticleDto;
+import com.springboot.board.dto.ArticleWithCommentsDto;
+import com.springboot.board.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
+        return Page.empty();
+    }
+    @Transactional(readOnly = true)
+    public ArticleWithCommentsDto getArticle(Long articleId) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+
+    }
+
+    public void updateArticle(ArticleDto dto) {
+    }
+
+    public void deleteArticle(long articleId) {
+    }
+
+    public Page<ArticleDto> searchArticlesViaHashtag(String hashtag, Pageable pageable) {
+        return null;
+    }
+
+    public List<String> getHashtags() {
+        return null;
+    }
+
+}
