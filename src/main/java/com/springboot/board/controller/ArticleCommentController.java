f3b194749652a7fc261d78ea2903269870b8912b
MJ0501 <mjay.do@gmail.com>
2024-12-15 16:06:59 +0900

    comment view 구현

--- src/main/java/com/springboot/board/controller/ArticleCommentController.java
+++ src/main/java/com/springboot/board/controller/ArticleCommentController.java
@@ -1,4 +1,35 @@
 package com.springboot.board.controller;
 
+import com.springboot.board.dto.UserAccountDto;
+import com.springboot.board.dto.request.ArticleCommentRequest;
+import com.springboot.board.service.ArticleCommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping("/comments")
+@Controller
 public class ArticleCommentController {
+    private final ArticleCommentService articleCommentService;
+
+    // Create Comment
+    @PostMapping("/new")
+    public String createNewArticleComment(ArticleCommentRequest articleCommentRequest){
+        //TODO 인증 구현하면 인증 정보 넘기기
+        articleCommentService.saveArticleComment(articleCommentRequest.toDto(UserAccountDto.of(
+                "MJ","m0501","MJ@mail.com","MJ","임시접근관리자")));
+        return "redirect:/articles/"+ articleCommentRequest.articleId();
+    }
+
+    // Delete Comment
+    @PostMapping("/{commentId}/delete")
+    public String deleteArticleComment(@PathVariable Long commentId, Long articleId){
+        //TODO 인증 구현하면, 인증정보 넣기
+        articleCommentService.deleteArticleComment(commentId);
+        return "redirect:/articles/"+articleId;
+    }
+
 }
---
+++ src/test/java/com/springboot/board/controller/ArticleCommentControllerTest.java
@@ -0,0 +1,77 @@
+package com.springboot.board.controller;
+
+import com.springboot.board.config.SecurityConfig;
+import com.springboot.board.dto.ArticleCommentDto;
+import com.springboot.board.dto.request.ArticleCommentRequest;
+import com.springboot.board.service.ArticleCommentService;
+import com.springboot.board.util.FormDataEncoder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Map;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@DisplayName("컨트롤러View - Comment")
+@Import({SecurityConfig.class, FormDataEncoder.class})
+@WebMvcTest(ArticleCommentController.class)
+class ArticleCommentControllerTest {
+
+    private final MockMvc mvc;
+    @MockBean
+    private ArticleCommentService articleCommentService;
+    private final FormDataEncoder formDataEncoder;
+
+    public ArticleCommentControllerTest(@Autowired MockMvc mvc, @Autowired FormDataEncoder formDataEncoder) {
+        this.mvc = mvc;
+        this.formDataEncoder = formDataEncoder;
+    }
+
+    // CREATE COMMENT
+    @DisplayName("[POST]/comments/new : Save new Comment")
+    @Test
+    void givenNewCommentInfo_whenRequesting_thenSavesNewComment() throws Exception {
+        Long articleId=1L;
+        ArticleCommentRequest articleCommentRequest = ArticleCommentRequest.of(articleId,"TESTTEST");
+        willDoNothing().given(articleCommentService).saveArticleComment(any(ArticleCommentDto.class));
+
+        mvc.perform(post("/comments/new")
+                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .content(formDataEncoder.encode(articleCommentRequest))
+                    .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/articles/"+articleId))
+                .andExpect(redirectedUrl("/articles/"+articleId));
+        then(articleCommentService).should().saveArticleComment(any(ArticleCommentDto.class));
+
+    }
+    // DELETE COMMENT
+    @DisplayName("[GET]/comments/{commentId}/delete : DELETE COMMENT")
+    @Test
+    void givenCommentId_thenDeletesComment() throws Exception {
+        long articleId = 1L;
+        long articleCommentId = 1L;
+        willDoNothing().given(articleCommentService).deleteArticleComment(articleCommentId);
+
+        mvc.perform(post("/comments/"+articleCommentId+"/delete")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .content(formDataEncoder.encode(Map.of("articleId",articleId)))
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/articles/"+articleId))
+                .andExpect(redirectedUrl("/articles/"+articleId));
+
+        then(articleCommentService).should().deleteArticleComment(articleCommentId);
+    }
+
+}
