f5ef651e2df412dee58b55214e8e6f6b8a3b547f
MJ0501 <mjay.do@gmail.com>
2024-12-17 13:33:06 +0900

    인증작업 - (임시)비번암호화해제
    개발용 data의 password의 암호화를 해제함

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 -- 테스트 계정
 -- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-('MJ', 'm0501', 'MJ', 'MJ@mail.com', 'I am MJ.', now(), 'MJ', now(), 'MJ')
+('MJ', '{noop}m0501', 'MJ', 'MJ@mail.com', 'I am MJ.', now(), 'MJ', now(), 'MJ')
 ;
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-('MJ2', 'm0501', 'MJ2', 'MJ2@mail.com', 'I am MJ2.', now(), 'MJ2', now(), 'MJ2')
+('MJ2', '{noop}m0501', 'MJ2', 'MJ2@mail.com', 'I am MJ2.', now(), 'MJ2', now(), 'MJ2')
 ;
 
 -- 123 게시글
